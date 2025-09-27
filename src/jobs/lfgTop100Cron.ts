6dd7a7fd78d849b0bb85af7c904e8d9fc656d888
DigitalDominance <digitaldominanceseo@gmail.com>
2025-09-27 18:37:14 -0400

    Update lfgTop100Cron.ts

--- src/jobs/lfgTop100Cron.ts
+++ src/jobs/lfgTop100Cron.ts
@@ -3,7 +3,7 @@ const cron = require('node-cron');
 const LFGToken = require('../models/LFGToken');
 const LFGTokenPrice = require('../models/LFGTokenPrice');
 
-const LFG_BASE = 'https://api.dev-lfg.kaspa.com/tokens/search';
+const LFG_BASE = 'https://api.lfg.kaspa.com/tokens/search';
 const DEFAULT_SORT = 'Market Cap (High to Low)';
 
 const doFetch = async (url: any) => {
